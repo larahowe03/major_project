6663752fe7ebd835c5f9266fe7e6e7ffe8d31dbb
Lara Howe <lara.howe@accenture.com>
2025-10-25 14:36:28 +1100

    results

--- tb/PATTERN_RECOGNITION/mif_to_image.py
+++ tb/PATTERN_RECOGNITION/mif_to_image.py
@@ -195,4 +195,4 @@ if __name__ == "__main__":
     else:
         # Default: convert output_image.mif from your testbench
         print("Converting output_image.mif to PNG...")
-        mif_to_grayscale_image("edge_very_aggressive.mif", "edge_very_aggressive.png")
+        mif_to_grayscale_image("verilog_results/present_edge_aggressive.mif", "verilog_results/present_edge_aggressive.png")
